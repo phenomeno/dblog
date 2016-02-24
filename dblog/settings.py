7fe92c9c51de645a562f4c64d0e5ab63106d4444
Grace Lee <writegracelee@gmail.com>
2016-02-23 22:10:39 -0800

    Changed good song in about section and set secret key to grab it from environment variable.

--- dblog/settings.py
+++ dblog/settings.py
@@ -15,12 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0*9+0r8vjp#b)g@)5v)lqbrdh%jj+4-_jt*t__36b2cst18w%^'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
